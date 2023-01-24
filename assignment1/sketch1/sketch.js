307b190fc336073972badbdabec733584184f761
connormo0221 <123109400+connormo0221@users.noreply.github.com>
2023-01-24 15:20:21 -0600

    sketch 1 complete

--- assignment1/sketch1/sketch.js
+++ assignment1/sketch1/sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
+  // using HSB color mode
+  colorMode(HSB);
 }
 
 function draw() {
-  background(220);
+  background(122, 100, 100);
+  fill(255);
+  strokeWeight(2);
+  circle(100, 100, 175);
+  square(210, 12.5, 175);
 }
