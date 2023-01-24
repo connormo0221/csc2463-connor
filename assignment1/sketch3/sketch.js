f460ca1bf34966226d135da4edd02bbddc2391e6
connormo0221 <123109400+connormo0221@users.noreply.github.com>
2023-01-24 16:12:31 -0600

    sketch 3 complete

--- assignment1/sketch3/sketch.js
+++ assignment1/sketch3/sketch.js
@@ -1,7 +1,35 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  noStroke();
+  
+  // drawing pacman
+  push();
+  fill(255, 255, 0);
+  arc(100, 100, 150, 150, PI + QUARTER_PI, -QUARTER_PI - PI);
+  pop();
+
+  // drawing body of ghost
+  push();
+  fill(255, 0, 0);
+  arc(300, 100, 150, 150, -PI, 0);
+  rect(225, 100, 150, 75);
+  pop();
+
+  // drawing eyes
+  push();
+  fill(255);
+  circle(265, 100, 50);
+  circle(335, 100, 50);
+  pop;
+
+  // drawing pupils
+  push();
+  fill(0, 0, 255);
+  circle(265, 100, 30);
+  circle(335, 100, 30);
+  pop;
 }
