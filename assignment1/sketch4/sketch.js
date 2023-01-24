6dc58f166b8e81568d8f3d0993edc3aee3b26558
connormo0221 <123109400+connormo0221@users.noreply.github.com>
2023-01-24 16:40:08 -0600

    sketch 4 complete

--- assignment1/sketch4/sketch.js
+++ assignment1/sketch4/sketch.js
@@ -3,5 +3,30 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(0, 0, 127.5);
+  stroke(255);
+  strokeWeight(5);
+
+  // drawing circle
+  push();
+  fill(0, 127.5, 0);
+  circle(200, 200, 200);
+  pop();
+
+  // drawing star
+  push();
+  fill(255, 0, 0);
+  beginShape();
+  vertex(100, 175);
+  vertex(175, 175);
+  vertex(200, 100);
+  vertex(225, 175);
+  vertex(300, 175);
+  vertex(240, 215);
+  vertex(260, 280);
+  vertex(200, 240);
+  vertex(140, 280);
+  vertex(160, 215);
+  endShape(CLOSE);
+  pop();
 }
