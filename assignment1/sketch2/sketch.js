993d6a5d897a4509a0927e2ddb30f2a457082dc4
connormo0221 <123109400+connormo0221@users.noreply.github.com>
2023-01-24 15:38:58 -0600

    sketch 2 complete

--- assignment1/sketch2/sketch.js
+++ assignment1/sketch2/sketch.js
@@ -1,7 +1,27 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 500);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  noStroke();
+  
+  // red circle
+  push();
+  fill(255, 0, 0, 127.5);
+  circle(300, 150, 250);
+  pop();
+
+  // blue circle
+  push();
+  fill(0, 255, 0, 127.5);
+  circle(400, 300, 250);
+  pop();
+  
+  // green circle
+  push();
+  fill(0, 0, 255, 127.5);
+  circle(200, 300, 250);
+  pop();
+  
 }
