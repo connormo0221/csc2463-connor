657105e90492b9311f5ba0dfddd65887d993c222
connormo0221 <123109400+connormo0221@users.noreply.github.com>
2023-02-07 15:12:08 -0600

    finished assignment3

--- assignment3/sketch.js
+++ assignment3/sketch.js
@@ -2,11 +2,10 @@ let guySprite;
 let greenSprite;
 let vikingSprite;
 
-let guyWalk;
-let greenWalk;
-let vikingWalk;
+let characters;
 
 function preload() {
+  // loading all images
   guySprite = loadImage("assets/SpelunkyGuy.png");
   greenSprite = loadImage("assets/Green.png");
   vikingSprite = loadImage("assets/Viking.png");
@@ -16,77 +15,91 @@ function setup() {
   createCanvas(800, 800);
   imageMode(CENTER);
 
-  guyWalk = new WalkingAnimation(guySprite, 80, 80, 200, 200, 9);
-  greenWalk = new WalkingAnimation(greenSprite, 80, 80, 100, 300, 9);
-  vikingWalk = new WalkingAnimation(vikingSprite, 80, 80, 50, 100, 9);
+  // manually setting framerate to prevent issues
+  frameRate(60);
+  
+  // initializing the characters
+  characters = [
+    new Character(guySprite, 400, 400),
+    new Character(greenSprite, 100, 200),
+    new Character(vikingSprite, 500, 600),
+    new Character(guySprite, -30, 400),
+    new Character(greenSprite, 600, 100),
+    new Character(vikingSprite, 200, 700)
+  ];
 }
 
 function draw() {
   background(220);
-
-  guyWalk.draw();
-  greenWalk.draw();
-  vikingWalk.draw();
+  for(i = 0; i < characters.length; i++) {
+    characters[i].draw();
+  }
 }
 
 function keyPressed() {
-  guyWalk.keyPressed(RIGHT_ARROW, LEFT_ARROW);
-  greenWalk.keyPressed(RIGHT_ARROW, LEFT_ARROW);
-  vikingWalk.keyPressed(RIGHT_ARROW, LEFT_ARROW);
+  for(i = 0; i < characters.length; i++) {
+    characters[i].keyPressed();
+  }
 }
 
 function keyReleased() {
-  guyWalk.keyReleased(RIGHT_ARROW, LEFT_ARROW);
-  greenWalk.keyReleased(RIGHT_ARROW, LEFT_ARROW);
-  vikingWalk.keyReleased(RIGHT_ARROW, LEFT_ARROW);
+  for(i = 0; i < characters.length; i++) {
+    characters[i].keyReleased();
+  }
 }
 
-class WalkingAnimation {
-  constructor(spriteSheet, sw, sh, dx, dy, animationLength, offsetX = 0, offsetY = 0) {
+class Character {
+  constructor(spriteSheet, dx, dy) {
     this.spriteSheet = spriteSheet;
-    this.sw = sw;
-    this.sh = sh;
     this.dx = dx;
     this.dy = dy;
-    this.u = 0;
-    this.v = 0;
-    this.animationLength = animationLength;
+
+    this.sxMulti = 0;
     this.currentFrame = 0;
     this.moving = 0;
     this.xDirection = 1;
-    this.offsetX = offsetX;
-    this.offsetY = offsetY;
   }
 
   draw() {
-    this.u = (this.moving != 0) ? this.currentFrame % this.animationLength : 0;
+    // set sx multiplier to switch character sprite if character is moving or to reset if not moving
+    if (this.moving != 0) {
+      this.sxMulti = this.currentFrame % 9;
+    } else {
+      this.sxMulti = 0;
+    }
+    
+    // drawing the character
     push();
     translate(this.dx, this.dy);
     scale(this.xDirection, 1);
-  
-    image(this.spriteSheet, 0, 0, this.sw, this.sh, this.u * this.sw + this.offsetX, this.v * this.sh + this.offsetY, this.sw, this.sh);
+    image(this.spriteSheet, 0, 0, 80, 80, this.sxMulti * 80, 0, 80, 80);
     pop();
+    
+    // incrementing animation frame based on # of current frame
     if (frameCount % 6 == 0) {
       this.currentFrame++;
     }
-  
+
+    // modifying dx based on amount of movement for translate() 
     this.dx += this.moving;
   }
 
-  keyPressed(right, left) {
-    if (keyCode == right) {
+  keyPressed() {
+    // set character to move right or left depending on key press
+    if (keyCode == RIGHT_ARROW) {
       this.moving = 1;
       this.xDirection = 1;
       this.currentFrame = 1;
-    } else if (keyCode == left) {
+    } else if (keyCode == LEFT_ARROW) {
       this.moving = -1;
       this.xDirection = -1;
       this.currentFrame = 1;
     }
   }
 
-  keyReleased(right, left) {
-    if (keyCode == right || keyCode == left) {
+  keyReleased() {
+    // set character to stop moving if key released
+    if (keyCode == RIGHT_ARROW || keyCode == LEFT_ARROW) {
       this.moving = 0;
     }
   }
